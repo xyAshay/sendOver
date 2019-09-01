5c41bdfc80f0d5fd8bc6b27b87517c93414e41a1
xyAshay <ashay.mailbox@gmail.com>
2019-09-01 22:26:02 +0530

    Basic Checks

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require('express');
+const fs = require('fs');
 const ip = require('ip');
 
 const app = express();
+const upDir = __dirname + '/uploads';
+var fPath;
 
 app.get('/', (req,res) => {
   res.send('Hello World!');
-})
-
-app.listen(3000,ip.address(), () => {
-  console.log(`Server Listening On http://${ip.address()}:3000`);
 });
+
+if(!fs.existsSync(upDir)){
+  fs.mkdir(upDir, (err) => {
+    if(err)
+      throw err;
+  });
+  console.log('Place The Files In The /uploads Directory And Try Again.');
+  process.exit();
+}
+else{
+  if(process.argv.length < 3){
+    console.log('Invalid Arguments Check Usage.');
+    console.log('Usage : Node index.js fileName.txt');
+  }
+  else{
+    const fName = process.argv[2];
+    fPath = upDir +'/'+ fName;
+    if(!fs.existsSync(fPath)){
+      console.log('File Does Not Exist!');
+      process.exit();
+    }
+    else{
+      if(!fs.lstatSync(fPath).isDirectory())
+        console.log('Serve...');
+      else
+        console.log('Directory Support Unavailable.');
+    }
+    app.listen(3000,ip.address(), () => {
+    console.log(`Server Listening On http://${ip.address()}:3000`);
+    });
+  }
+}
